69193712a3992f87bb4e41108e04ab858dcf858d
ditobprasetio <ditobagus28@gmail.com>
2021-03-24 22:13:31 +0700

    add client

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="currentPage === 'Dashboard'">
+    <h1>Dashboard</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+  props: ['currentPage'],
+  data() {
+    return {}
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
